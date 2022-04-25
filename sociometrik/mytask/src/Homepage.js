0bc32d07d4234bc9b2db858dae44be9dbabf5d8d
sonikasoni <sonika.soni@afinoz.com>
2022-04-25 16:21:08 +0530

    Init

---
+++ sociometrik/mytask/src/Homepage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Homepage extends Component {
+  render() {
+    return (
+      <div className="boundary">
+<h2 style={{textAlign:"center"}}>Here is the map of Uttar Pradesh</h2>
+<div class="mapouter">
+    <div class="gmap_canvas">
+        <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=uttar%20pradesh%20india&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+            </iframe><a href="https://putlocker-is.org"></a>
+          <a href="https://www.embedgooglemap.net">embedgooglemap.net</a></div></div>
+      </div>
+    )
+  }
+}
+
+export default Homepage
